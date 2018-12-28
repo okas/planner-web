fc23cfa345900da7b7a5ef374b9f52194014b77d
Lauri Saar <Lauri.Saar@telia.ee>
2018-12-28 04:50:15 +0200

    Changed: dependency component import uses .vue extenson (code style)

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import AppFooter from './components/AppFooter'
+import AppFooter from './components/AppFooter.vue'
 export default {
   name: 'App',
   components: { AppFooter },
