678cee9c9d74214a6a22909e937cc49a18628fc5
Lauri Saar <Lauri.Saar@telia.ee>
2018-12-28 21:51:48 +0200

    Changed: refactored top navigation to SFC.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <nav id="app-top-nav">
-      <router-link to="/" exact>Kodu</router-link>&nbsp;|
-      <router-link to="/devices/" replace>Seadmed</router-link>
-    </nav>
+    <app-top-nav/>
     <router-view id="app-content"/>
     <app-footer id="app-footer"/>
   </div>
 </template>
 
 <script>
+import AppTopNav from './components/AppTopNav.vue'
 import AppFooter from './components/AppFooter.vue'
 export default {
   name: 'App',
-  components: { AppFooter },
+  components: { AppTopNav, AppFooter },
   head: {
     link() {
       // Move to main.js or somewhere when it gets rendered in server rather than in browser
@@ -37,25 +35,10 @@ export default {
 
 <style lang="scss">
 @import '@globalImpScss';
-
 #app {
   font-family: $family-sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: $default-text-color;
-}
-#app-top-nav {
-  padding: 30px;
-  border: 1px solid #bcedef;
-  border-bottom-left-radius: 18px;
-  border-bottom-right-radius: 18px;
-  box-shadow: inset 0px 0px 20px 15px #c5deff;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.is-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/AppTopNav.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav id="app-top-nav">
+    <router-link to="/" exact>Kodu</router-link>&nbsp;|
+    <router-link to="/devices/" replace>Seadmed</router-link>
+  </nav>
+</template>
+
+<style lang="scss">
+#app-top-nav {
+  padding: 30px;
+  border: 1px solid #bcedef;
+  border-bottom-left-radius: 18px;
+  border-bottom-right-radius: 18px;
+  box-shadow: inset 0px 0px 20px 15px #c5deff;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.is-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
