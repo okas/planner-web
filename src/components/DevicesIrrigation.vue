26b8f00765054e1d7afc3455e57c16ed60920c80
Lauri Saar <Lauri.Saar@telia.ee>
2018-12-28 04:45:30 +0200

    Added dummny files: - reflects upcoming structure of site - for automatic side menu testing (upcoming)

---
+++ src/components/DevicesIrrigation.vue
@@ -0,0 +1,12 @@
+<template>
+  <main id="irrigation">
+    <h1 class="title is-1 has-text-info">Taimede kastmine</h1>
+    <p class="has-text-info">Kastmise süsteemi armatuurlaud</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'DevicesIrrigation'
+}
+</script>
---
+++ src/components/DevicesWindowBlinds.vue
@@ -0,0 +1,11 @@
+<template>
+  <main id="windowlinds">
+    <h1 class="title is-1 has-text-success">Ruloode juhtimine</h1>
+    <p class="has-text-success">Roloode süsteemi armatuurlaud</p>
+  </main>
+</template>
+<script>
+export default {
+  name: 'DevicesWindowBlinds'
+}
+</script>
