860b7ae15d34b1567fec1ef600d86ea9c5f3afab
Lauri Saar <Lauri.Saar@telia.ee>
2018-12-30 13:29:13 +0200

    .Changed: tured HTTP400 SFC message to Bulma notification.

--- src/views/HTTP404.vue
+++ src/views/HTTP404.vue
@@ -1,7 +1,7 @@
 <template functional>
   <main id="http400" class="has-text-centered">
     <h2 class="title is-3 is-spaced has-text-weight-semibold">HTTP 404</h2>
-    <p class="has-background-warning">Siin aadresil ei ole midagi</p>
+    <p class="notification is-warning">Siin aadresil ei ole midagi</p>
     <pre><i>{{props.windowLocationOrigin}}</i><b>{{parent.$route.fullPath}}</b></pre>
   </main>
 </template>
