9f266ef0ebf6b5a9e7ec446fb82551d63c9fb1fd
Lauri Saar <Lauri.Saar@telia.ee>
2018-12-30 13:25:48 +0200

    .Changed: navbar leans mor to Bulma-only styling -- thus removing customizations. - simplified HTML structure.

--- src/components/AppTopNav.vue
+++ src/components/AppTopNav.vue
@@ -7,8 +7,8 @@
   >
     <div class="navbar-brand">
       <router-link class="navbar-item is-tab" to="/" exact>
-        <span class>Saar</span>
-        <img class="app_logo" src="../assets/logo_tk.png">
+        Saar
+        <img class="app-logo" src="../assets/logo_tk.png">
       </router-link>
       <a
         class="navbar-burger burger"
@@ -23,6 +23,8 @@
         <span aria-hidden="true"/>
       </a>
     </div>
+    <!-- Links from router should be rendered based on rout configuration.
+    Like MenuSide component.-->
     <div id="navbar-menu" class="navbar-menu" :class="activeCls">
       <div class="navbar-start">
         <router-link class="navbar-item is-tab" to="/devices">Seadmed</router-link>
@@ -72,26 +74,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-#app-top-nav {
-  // border: 1px solid #bcedef;
-  // border-bottom-left-radius: 15px;
-  // border-bottom-right-radius: 15px;
-  // box-shadow: inset 0px 0px 20px 8px #c5deff;
-  .navbar-item,
-  .navbar-item > * {
-    font-weight: bold;
-    // color: #2c3e50;
-    &.router-link-active {
-      color: #42b983;
-    }
-  }
-  // .app_logo {
-  //   height: 40px;
-  // }
-  .title {
-    vertical-align: middle;
-  }
-}
-</style>
