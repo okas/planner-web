715d981d6a2a3c11b171378bf75047a85091d3df
Lauri Saar <Lauri.Saar@telia.ee>
2018-12-30 04:10:40 +0200

    .Adde Bulma Navbar component styling. - Not quite done. - Commented out some code for future (burger).

--- src/components/AppTopNav.vue
+++ src/components/AppTopNav.vue
@@ -1,23 +1,51 @@
 <template>
-  <nav id="app-top-nav">
-    <router-link to="/" exact>Kodu</router-link>&nbsp;|
-    <router-link to="/devices/" replace>Seadmed</router-link>
+  <nav id="app-top-nav" class="navbar is-light" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="/" exact>
+        <h2 class>Saar</h2>
+        <img class="app_logo" src="../assets/logo_tk.png">
+      </router-link>
+      <router-link class="navbar-item" to="/devices">Seadmed</router-link>
+      <router-link class="navbar-item" to="/about">Info</router-link>
+      <!-- <button
+        class="navbar-burger burger button"
+        role="button"
+        aria-expanded="false"
+        data-target="navbar-menu"
+      >
+        <span aria-hidden="true"/>
+        <span aria-hidden="true"/>
+        <span aria-hidden="true"/>
+      </button>-->
+    </div>
+    <!-- <div id="navbar-menu" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link class="navbar-item" to="/devices">Seadmed</router-link>
+        <router-link class="navbar-item" to="/about">Info</router-link>
+      </div>
+    </div>-->
   </nav>
 </template>
 
 <style lang="scss">
 #app-top-nav {
-  padding: 30px;
   border: 1px solid #bcedef;
-  border-bottom-left-radius: 18px;
-  border-bottom-right-radius: 18px;
-  box-shadow: inset 0px 0px 20px 15px #c5deff;
-  a {
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  box-shadow: inset 0px 0px 20px 8px #c5deff;
+  .navbar-item,
+  .navbar-item > * {
     font-weight: bold;
     color: #2c3e50;
-    &.is-active {
+    &.router-link-active {
       color: #42b983;
     }
+  }
+  // .app_logo {
+  //   height: 40px;
+  // }
+  .title {
+    vertical-align: middle;
   }
 }
 </style>
