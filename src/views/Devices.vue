422ae53563ce558cefc4660a8c79db441431f804
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-01 07:14:27 +0200

    .Changed: removed css is-fluid modifier as it falls out of container of "document".

--- src/views/Devices.vue
+++ src/views/Devices.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <div class="container is-fluid">
+    <div class="container">
       <div class="columns">
         <aside class="column is-narrow">
           <MenuSide :selected-paths="path"/>
