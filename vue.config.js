c4e963c2a730e3578aa0cb52dc9659439f88f50c
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-04 17:55:18 +0200

    .Added: socket.io-client NPM package install.
    .forDev: vue.config has proxy set up for socket.io API development.

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
   assetsDir: undefined,
   productionSourceMap: undefined,
   parallel: undefined,
+  lintOnSave: undefined,
   css: {
     loaderOptions: {
       sass: {
@@ -15,14 +16,21 @@ module.exports = {
       }
     }
   },
+  devServer: {
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        ws: true
+        // logLevel: 'debug'
+      }
+    }
+  },
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       // mutate config for production...
     } else if (process.env.NODE_ENV === 'development') {
-      // mutate for development...
       config.devtool = 'eval-source-map' // test instead of 'source-map'
     }
-  },
-
-  lintOnSave: undefined
+  }
 }
