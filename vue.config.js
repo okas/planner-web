ce84f1dac2b278afdd3b8e0afeecae80dcbdd092
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-07 21:04:42 +0200

    .Chnaged: experimenting with alternative socket.io path.

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   devServer: {
     proxy: {
-      '/socket.io': {
+      '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         ws: true
