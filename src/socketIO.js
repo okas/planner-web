889b62a8132e549b656550f5364472836c0826ce
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-09 02:32:26 +0200

    .Added: default config for vue-socket.io plugin, with enforcement that connection key will be null and set to initialized socket instance. - vuex store will be retreivd by module inport by default.
    .Changed: both vue-socket.io and -client enforcement code refactored to funtions.
    - both have decent initial values  suitable for project.

--- src/socketIO.js
+++ src/socketIO.js
@@ -1,25 +1,24 @@
 import Vue from 'vue'
 import SocketIOClient from 'socket.io-client'
 import VueSocketIO from 'vue-socket.io'
+import store from './store'
 
 const mode = process.env.NODE_ENV !== 'production'
+let isInitialized = false
 
-export default function init(
-  socketIoConfig = {
-    path: '/api',
-    transports: ['websocket', 'polling'],
-    uri: '/'
-  }
-) {
-  if (socketIoConfig.autoConnect == true) {
-    console.warn(
-      `! autoConnect: "${
-        socketIoConfig.autoConnect
-      }": socket.io is connected just after setting up all the events, this setting will be enforced to "false".`
-    )
-    socketIoConfig.autoConnect = false
-  }
+/* Initial configs */
 
+const initialSocketIoClientConfig = {
+  path: '/api',
+  transports: ['websocket', 'polling'],
+  uri: '/'
+}
+const initialVueSocketIoConfig = {
+  debug: mode,
+  vuex: { store, actionPrefix: 'a_io_', mutationPrefix: 'm_io_' }
+}
+
+function registerSocketEvents(socket) {
   let id = undefined
   const socket = SocketIOClient('/', socketIoConfig)
 
@@ -30,11 +29,6 @@ export default function init(
     socket.io.opts.transports = ['polling', 'websocket']
   })
 
-  socket.on('connect', () => {
-    id = socket.id
-    console.info(`%c|-> [ ${id} ] : connected with id.`, 'color:purple')
-  })
-
   socket.on('disconnect', reason => {
     console.info(`%c>-| [ ${id} ] : reason: "${reason}"`, 'color:purple')
     // Be more forceful here ? Transport errors will make it give up..
@@ -42,13 +36,54 @@ export default function init(
       socket.connect()
     }
   })
+}
 
+function enforceSocketIoClientConfig(config) {
+  if (config.autoConnect == true) {
+    console.warn(
+      `! socket.io-client, autoConnect: "${
+        config.autoConnect
+      }": socket.io is connected just after setting up all the events, this setting will be enforced to "false".`
+    )
+  }
+  config.autoConnect = false
+}
+
+function enforceVueSocketIoConfig(config) {
+  if (config.connection) {
+    console.warn(
+      `! vue-socket.io, connection: "${
+        config.connection
+      }": use param "socketIoclientConfig" to configure socket.io-client.`
+    )
+  }
+  config.connection = null
+}
+
+/* Using init function to inject options to this module */
+
+export default function init(
+  vueSocketIoConfig = {},
+  socketIoClientConfig = {}
+) {
+  /* Allow init at most once. */
+  if (isInitialized) {
+    return
+  }
+  enforceVueSocketIoConfig(vueSocketIoConfig)
+  enforceSocketIoClientConfig(socketIoClientConfig)
+  const socket = SocketIOClient({
+    ...initialSocketIoClientConfig,
+    ...socketIoClientConfig
+  })
+  registerSocketEvents(socket)
   socket.connect()
-
   Vue.use(
     new VueSocketIO({
-      debug: mode,
-      connection: socket
+      ...initialVueSocketIoConfig,
+      ...vueSocketIoConfig,
+      ...{ connection: socket }
     })
   )
+  isInitialized = true
 }
