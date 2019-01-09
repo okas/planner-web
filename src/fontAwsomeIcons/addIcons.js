a1135360efbb9337f8a418468f815bf690b49dc4
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-09 19:05:31 +0200

    .Changed: refactored fontAwsomeIcons to module folder: - separated icons registration to own module, as it is expected to get long over time.

---
+++ src/fontAwsomeIcons/addIcons.js
@@ -0,0 +1,5 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faLightbulb)
--- src/fontAwsomeIcons/index.js
+++ src/fontAwsomeIcons/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import './addIcons'
 
 export default function initIcons(
   fontAwsomeIconsComponentName = 'font-awesome-icon'
 ) {
-  library.add(faLightbulb)
   Vue.component(fontAwsomeIconsComponentName, FontAwesomeIcon) // component name is used in templates
 }
