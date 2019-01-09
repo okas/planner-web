cd597c7622deb2968fbcdd00b4b72973a4056e46
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-09 02:35:00 +0200

    .Added: initial implementation of store: - holds connectivity status of API. - status set via vue-socket.io vuex integration, mutations.

--- src/store.js
+++ src/store.js
@@ -4,7 +4,12 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    apiConnected: undefined
+  },
+  mutations: {
+    m_io_connect: s => (s.apiConnected = true),
+    m_io_disconnect: s => (s.apiConnected = false)
+  },
   actions: {}
 })
