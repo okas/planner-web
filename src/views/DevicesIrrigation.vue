b0f42d3cdc372010f37ebb85b77c79b1e9d98750
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-11 21:11:07 +0200

    .forDev: seems like there are no visible madifications, though Git telst there is something...

--- src/views/DevicesIrrigation.vue
+++ src/views/DevicesIrrigation.vue
@@ -2,7 +2,7 @@
   <section id="irrigation">
     <header>
       <slot name="header-title" :cls="'has-text-info'"/>
-    <p class="has-text-info">Kastmise süsteemi armatuurlaud</p>
+      <p class="has-text-info">Kastmise süsteemi armatuurlaud</p>
     </header>
   </section>
 </template>
