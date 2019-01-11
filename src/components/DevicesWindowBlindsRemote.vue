4a6c1dd2f3cad9071687286a12b1cbd9d1601bc7
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-11 21:57:07 +0200

    .Changed: improved blind remote SFC's layout: - fully relying on CSS Grid! - slider rotation uses now automatic calculations for transform-origin CSS prop, - changed some class names and simplified HTML structure of template.

--- src/components/DevicesWindowBlindsRemote.vue
+++ src/components/DevicesWindowBlindsRemote.vue
@@ -1,28 +1,26 @@
 <template>
-  <div class="remote">
-    <div class="box">
-      <h4 class="subtitle is-4 has-text-centered" v-text="blind.name"/>
-      <div class="controls">
-        <button class="button up">
-          <f-a icon="chevron-up"/>
-        </button>
-        <button class="button stop">
-          <f-a :icon="['far', 'square']"/>
-        </button>
-        <button class="button down">
-          <f-a icon="chevron-down"/>
-        </button>
-        <div class="slider-wrapper">
-          <div class="wrap-helper">
-            <input
-              type="range"
-              class="slider is-large is-circle"
-              step="0.1"
-              min="0"
-              max="1"
-              :value="blind.state"
-            >
-          </div>
+  <div class="blind-remote box">
+    <div class="grid">
+      <h4 class="remote-label is-size-5 has-text-centered" v-text="blind.name"/>
+      <button class="up button is-medium">
+        <f-a icon="chevron-up"/>
+      </button>
+      <button class="stop button is-medium">
+        <f-a :icon="['far', 'square']"/>
+      </button>
+      <button class="down button is-medium">
+        <f-a icon="chevron-down"/>
+      </button>
+      <div class="slider-wrapper">
+        <div class="helper">
+          <input
+            type="range"
+            class="slider is-large is-circle"
+            step="0.01"
+            min="0"
+            max="1"
+            :value="blind.state"
+          >
         </div>
       </div>
     </div>
@@ -36,41 +34,57 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.remote {
+$col-gap: 10px;
+$row-gap: 22.5px;
+$button-height: 45px;
+$button-width: 49.5px;
+$slider-height: $row-gap * 2 + $button-height * 3;
+// Following 3 lines calculate transform-origin values related to slider height
+$origin-offset: 5px;
+$x-origin: ($slider-height / 2 - $origin-offset);
+$y-origin: ($slider-height / 2 + $origin-offset);
+
+.blind-remote {
   display: inline-flex;
-  align-items: center;
   justify-content: center;
-  .controls {
+  .grid {
     display: grid;
+    justify-content: center;
     justify-items: center;
-    grid-template-columns: repeat(2, 40px);
-    grid-template-rows: repeat(3, 36px);
-    grid-row-gap: 18px;
+    grid-template-columns: repeat(2, $button-width);
+    grid-template-rows: 2rem repeat(3, $button-height);
+    grid-column-gap: $col-gap;
+    grid-row-gap: $row-gap;
     grid-template-areas:
+      'lb lb'
       'up sl'
       'st sl'
       'dn sl';
-    .button.up {
+    .remote-label {
+      grid-area: lb;
+      align-self: center;
+    }
+    .up {
       grid-area: up;
     }
-    .button.stop {
+    .stop {
       grid-area: st;
     }
-    .button.down {
+    .down {
       grid-area: dn;
     }
     .slider-wrapper {
       grid-area: sl;
-      .wrap-helper {
+      .helper {
         display: inline-block;
         width: 1px;
-        height: 144px;
+        height: $slider-height;
         padding: 0;
         .slider {
-          width: 144px;
+          width: $slider-height;
           height: 1px;
           margin: 0;
-          transform-origin: 68px 76px;
+          transform-origin: $x-origin $y-origin;
           transform: rotate(-90deg);
         }
       }
