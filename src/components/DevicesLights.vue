ed403b0d4afd16fd3a5698f7b419caeebe34875c
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-11 02:26:11 +0200

    .Changed: extracted "lamp" structure to own SFC.

--- src/components/DevicesLights.vue
+++ src/components/DevicesLights.vue
@@ -14,19 +14,7 @@
           v-for="l in lamps"
           :key="l.id"
         >
-          <div class="lamp box">
-            <h4 class="lamp-name is-size-6" v-text="l.name"/>
-            <f-a class="lamp-icon fa-2x" icon="lightbulb" :class="{'has-text-warning': !!l.state}"/>
-            <div class="switch-container">
-              <input
-                class="switch is-thin is-rounded"
-                type="checkbox"
-                :id="`l${l.id}`"
-                v-model="l.state"
-              >
-              <label class="switch-label" :for="`l${l.id}`"/>
-            </div>
-          </div>
+          <lamp :lamp="l"/>
         </li>
       </ul>
     </section>
@@ -34,6 +22,7 @@
 </template>
 
 <script>
+import Lamp from './DevicesLightsLamp'
 export default {
   data() {
     return {
@@ -41,6 +30,7 @@ export default {
       groupedLamps: []
     }
   },
+  components: { Lamp },
   methods: {
     ioGetAllLamps() {
       this.$socket.emit(
@@ -62,31 +52,5 @@ export default {
 }
 .room.section {
   padding: 0 0 2.5rem;
-  .lamp {
-    display: grid;
-    grid-column-gap: 0.5rem;
-    grid-row-gap: 0.25rem;
-    grid-template-areas:
-      'n n'
-      'i s';
-    grid-template-columns: 37.5% 63.25%;
-    justify-content: center;
-    &.box {
-      padding: 0.75rem;
-    }
-    .lamp-name {
-      grid-area: n;
-      justify-self: center;
-    }
-    .lamp-icon {
-      grid-area: i;
-      justify-self: end;
-    }
-    > .switch-container {
-      grid-area: s;
-      justify-self: start;
-      align-self: end;
-    }
-  }
 }
 </style>
---
+++ src/components/DevicesLightsLamp.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="lamp box">
+    <h4 class="lamp-name is-size-6" v-text="lamp.name"/>
+    <f-a class="lamp-icon fa-2x" icon="lightbulb" :class="{'has-text-warning': !!lamp.state}"/>
+    <div class="switch-container">
+      <input
+        class="switch is-thin is-rounded"
+        type="checkbox"
+        :id="`l${lamp.id}`"
+        v-model="lamp.state"
+      >
+      <label class="switch-label" :for="`l${lamp.id}`"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { lamp: { type: Object, required: true } }
+}
+</script>
+
+<style  lang="scss" scoped>
+.lamp {
+  display: grid;
+  grid-column-gap: 0.5rem;
+  grid-row-gap: 0.25rem;
+  grid-template-areas:
+    'n n'
+    'i s';
+  grid-template-columns: 37.5% 63.25%;
+  justify-content: center;
+  &.box {
+    padding: 0.75rem;
+  }
+  .lamp-name {
+    grid-area: n;
+    justify-self: center;
+  }
+  .lamp-icon {
+    grid-area: i;
+    justify-self: end;
+  }
+  > .switch-container {
+    grid-area: s;
+    justify-self: start;
+    align-self: end;
+  }
+}
+</style>
