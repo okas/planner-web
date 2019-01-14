7c20b392f0313a32bd7bb1dd48db108edf2a1897
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-14 20:17:24 +0200

    .Changed: simplified template structure.

--- src/components/DevicesWindowBlindsRemote.vue
+++ src/components/DevicesWindowBlindsRemote.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="blind-remote box">
-    <div class="grid">
-      <h4 class="remote-label is-size-5 has-text-centered" v-text="blind.name"/>
-      <button class="up button is-medium">
-        <f-a icon="chevron-up"/>
-      </button>
-      <button class="stop button is-medium">
-        <f-a :icon="['far', 'square']"/>
-      </button>
-      <button class="down button is-medium">
-        <f-a icon="chevron-down"/>
-      </button>
-      <div class="slider-wrapper">
-        <div class="helper">
-          <input
-            type="range"
-            class="slider is-large is-circle"
-            step="0.01"
-            min="0"
-            max="1"
-            :value="blind.state"
-          >
-        </div>
+  <div class="blind-remote-grid box is-marginless">
+    <h4 class="remote-label is-size-5 has-text-centered" v-text="blind.name"/>
+    <button class="up button is-medium">
+      <f-a icon="chevron-up"/>
+    </button>
+    <button class="stop button is-medium">
+      <f-a :icon="['far', 'square']"/>
+    </button>
+    <button class="down button is-medium">
+      <f-a icon="chevron-down"/>
+    </button>
+    <div class="slider-wrapper">
+      <div class="helper">
+        <input
+          type="range"
+          class="slider is-large is-circle"
+          step="0.01"
+          min="0"
+          max="1"
+          :value="blind.state"
+        >
       </div>
     </div>
   </div>
@@ -44,49 +42,49 @@ $origin-offset: 5px;
 $x-origin: ($slider-height / 2 - $origin-offset);
 $y-origin: ($slider-height / 2 + $origin-offset);
 
-.blind-remote {
-  display: inline-flex;
+.blind-remote-grid {
+  display: grid;
   justify-content: center;
-  .grid {
-    display: grid;
-    justify-content: center;
-    justify-items: center;
-    grid-template-columns: repeat(2, $button-width);
-    grid-template-rows: 2rem repeat(3, $button-height);
-    grid-column-gap: $col-gap;
-    grid-row-gap: $row-gap;
-    grid-template-areas:
-      'lb lb'
-      'up sl'
-      'st sl'
-      'dn sl';
-    .remote-label {
-      grid-area: lb;
-      align-self: center;
-    }
-    .up {
-      grid-area: up;
-    }
-    .stop {
-      grid-area: st;
-    }
-    .down {
-      grid-area: dn;
-    }
-    .slider-wrapper {
-      grid-area: sl;
-      .helper {
-        display: inline-block;
-        width: 1px;
-        height: $slider-height;
-        padding: 0;
-        .slider {
-          width: $slider-height;
-          height: 1px;
-          margin: 0;
-          transform-origin: $x-origin $y-origin;
-          transform: rotate(-90deg);
-        }
+  justify-items: center;
+  grid-template-columns: repeat(2, $button-width);
+  grid-template-rows: 2rem repeat(3, $button-height);
+  grid-column-gap: $col-gap;
+  grid-row-gap: $row-gap;
+  grid-template-areas:
+    'lb lb'
+    'up sl'
+    'st sl'
+    'dn sl';
+  &.box {
+    width: 149px;
+    height: 274.5px;
+  }
+  .remote-label {
+    grid-area: lb;
+    align-self: center;
+  }
+  .up {
+    grid-area: up;
+  }
+  .stop {
+    grid-area: st;
+  }
+  .down {
+    grid-area: dn;
+  }
+  .slider-wrapper {
+    grid-area: sl;
+    .helper {
+      display: inline-block;
+      width: 1px;
+      height: $slider-height;
+      padding: 0;
+      .slider {
+        width: $slider-height;
+        height: 1px;
+        margin: 0;
+        transform-origin: $x-origin $y-origin;
+        transform: rotate(-90deg);
       }
     }
   }
