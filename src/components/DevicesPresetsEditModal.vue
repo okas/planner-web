7fcfe90c3c0b62c729df1d7d8b9b16d8796cb322
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-19 00:49:23 +0200

    .Added: key event handler for Enter.
    .Added: bulma-steps (extension) to project:
    - SASS is imported from assets.
    - JS is imported to SFC directly.
    - added custom behavior via callback to dynamically control element focusing.
    - in case of no control elements in step's contents, then modal's root element gets focus to allow Enter and Esc key event capture.
    - added custom visibility handling via CSS for step's control buttons and markers.
    - last step-item e.g. finish has special CSS rules to control its appearance.
    - step-items are rendered by v-for.
    - added extension's initla SCSS variable to app's file.
    
    .Added: icons for steps.
    
    .Changed: improvements for Editor:
    - form is wrapped by steps plugin -- contents.

--- src/components/DevicesPresetsEditModal.vue
+++ src/components/DevicesPresetsEditModal.vue
@@ -1,26 +1,92 @@
 <template>
-  <div class="modal" tabindex="0" ref="esc" @keyup.stop.esc="$emit('quit')">
+  <div
+    id="devicesPresetEditor"
+    class="modal"
+    tabindex="0"
+    ref="root"
+    @keyup.stop.esc="$emit('quit')"
+    @keyup.stop.enter="$emit('save', p)"
+  >
     <div class="modal-background"></div>
     <div class="modal-content">
-      <div class="field">
-        <label class="label">Name</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="Text input" v-model="p.name">
-        </div>
+      <div class="control">
+        <button class="modal-close is-large" aria-label="close" @click="$emit('quit')"/>
       </div>
-      <div class="field">
-        <label class="label">Schedule</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="Text input" v-model="p.schedule">
+      <section class="steps">
+        <!-- render 3 steps based on viewmodel data -->
+        <div class="step-item" v-for="s in $static.steps" :key="s.id">
+          <div class="step-marker">
+            <span class="text" v-text="s.id"/>
+            <span class="icon">
+              <f-a :icon="s.icon"/>
+            </span>
+          </div>
+          <div class="step-details">
+            <p class="step-title" v-text="s.title"/>
+            <p v-text="s.description"/>
+          </div>
         </div>
-      </div>
+        <section class="steps-content">
+          <div class="step-content has-text-centered">
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label has-text-grey">Nimi</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <input class="input" type="text" placeholder="Text input" v-model="p.name">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label has-text-grey">Ajastus</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <input class="input" type="text" placeholder="Text input" v-model="p.schedule">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="step-content has-text-centered">Sisu 2</div>
+          <div class="step-content has-text-centered">Sisu 3</div>
+        </section>
+        <section class="steps-actions">
+          <div class="steps-action">
+            <button class="button is-outlined is-success" @click="$emit('save', p)">
+              <span class="icon">
+                <f-a icon="check"/>
+              </span>
+            </button>
+          </div>
+          <div class="steps-action">
+            <button class="button is-outlined is-light" data-nav="previous">
+              <span class="icon">
+                <f-a icon="chevron-left"/>
+              </span>
+            </button>
+          </div>
+          <div class="steps-action">
+            <button class="button is-outlined is-light" data-nav="next">
+              <span class="icon">
+                <f-a icon="chevron-right"/>
+              </span>
+            </button>
+          </div>
+        </section>
+      </section>
     </div>
-    <button class="button is-primary" @click="$emit('save', p)">Save</button>
-    <button class="modal-close is-large" aria-label="close" @click="$emit('quit')"></button>
   </div>
 </template>
 
 <script>
+import BulmaSteps from 'bulma-extensions/bulma-steps/src/js'
+
 export default {
   props: { preset: { type: Object, required: true } },
   data() {
@@ -28,10 +94,80 @@ export default {
       p: { ...this.preset }
     }
   },
+  static() {
+    return {
+      steps: [
+        {
+          id: 1,
+          title: 'Nimetus',
+          description: 'Määra nimi ja toimumise aja muster.',
+          icon: 'check'
+        },
+        {
+          id: 2,
+          title: 'Seadmed',
+          description: 'Vali seadmed, mida määratud ajal käitatakse.',
+          icon: 'check'
+        },
+        {
+          id: 3,
+          title: 'Järjestus',
+          description: 'Vali, millises järjekorras seadmed käivitatakse.',
+          icon: 'flag'
+        }
+      ]
+    }
+  },
   mounted() {
-    // Additional to HTML element focusing, you need to add [tabindex]
-    // as well to it to get keypress working on non-control elements!
-    this.$refs.esc.focus()
+    let stepsElement = document.querySelector(
+      `.steps[${this.$options._scopeId}]`
+    )
+    let contentsElements = stepsElement.querySelectorAll(
+      '.steps-content>.step-content'
+    )
+
+    /**
+     * Manipulate focus of some element in modal to caputer ESC key event.
+     * @param stepId 0-based index of .step-item element; they allign with .step-content elements.
+     */
+    function showHandler(stepId) {
+      if (!contentsElements[stepId]) {
+        return
+      }
+      let anyControl = contentsElements[stepId].querySelector(
+        'input[type="text"], button, input'
+      )
+      if (anyControl) {
+        anyControl.focus()
+      } else {
+        // Optionally could set focus to step elements as well if they are clickables
+        this.$refs.root.focus()
+      }
+    }
+    new BulmaSteps(stepsElement, {
+      // stepClickable: true, // experimenting, source has this option
+      /* need to add Vue instance as a context to handler */
+      onShow: showHandler.bind(this)
+    })
   }
 }
 </script>
+
+<style lang="scss" scoped>
+/* swaps .step-marker content: .text vs .icon */
+/* special behavior of last/finish step */
+/* remove unusable button */
+.step-item.is-completed .step-marker .text,
+.step-item:not(.is-completed) .step-marker .icon,
+.step-item:last-of-type.is-active .step-marker .text,
+.steps-action [data-nav][disabled] {
+  display: none;
+}
+.step-item:last-of-type.is-active .step-marker {
+  color: $success;
+  border-color: $success;
+}
+.step-item:last-of-type.is-active .step-marker .icon {
+  display: inherit;
+}
+</style>
