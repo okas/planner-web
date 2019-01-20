bed9fee99922d2c0e17dae2f8fe27f233f859a60
Lauri Saar <Lauri.Saar@telia.ee>
2019-01-20 17:32:00 +0200

    .Changed: log/info string refactoring.

--- src/plugins/socket.io-plugin/sysEvents.js
+++ src/plugins/socket.io-plugin/sysEvents.js
@@ -7,9 +7,9 @@ export default function register(socket) {
     console.info(`%c|-> [ ${id} ] : connected with id.`, `color:${color}`)
   })
 
-  socket.on('reconnect_attempt', () => {
+  socket.on('reconnect_attempt', attemptNumber => {
     console.warn(
-      '! socket.io is reconnectiong: set transports to polling+websocket just in case.'
+      `! socket.io is reconnectiong: [ ${attemptNumber} ] : set transports to polling+websocket just in case.`
     )
     socket.io.opts.transports = ['polling', 'websocket']
   })
@@ -19,30 +19,30 @@ export default function register(socket) {
   })
 
   socket.on('connect_error', error => {
-    console.log(`event: "connection_error" : [ ${error} ]`)
+    console.error(`socket.io event: "connection_error" : [ ${error} ]`)
   })
 
   socket.on('connect_timeout', timeout => {
-    console.log(`event: "connect_timeout" : [ ${timeout} ]`)
+    console.log(`socket.io event: "connect_timeout" : [ ${timeout} ]`)
   })
 
   socket.on('error', error => {
-    console.log(`event: "error" : [ ${error} ]`)
+    console.error(`socket.io event: "error" : [ ${error} ]`)
   })
 
   socket.on('reconnect', attemptNumber => {
-    console.log(`event: "reconnect" : [ ${attemptNumber} ]`)
+    console.warn(`socket.io event: "reconnect" : [ ${attemptNumber} ]`)
   })
 
   socket.on('reconnecting', attemptNumber => {
-    console.log(`event: "reconnecting" : [ ${attemptNumber} ]`)
+    console.warn(`socket.io event: "reconnecting" : [ ${attemptNumber} ]`)
   })
 
   socket.on('reconnect_error', error => {
-    console.log(`event: "reconnect_error" : [ ${error} ]`)
+    console.error(`socket.io event: "reconnect_error" : [ ${error} ]`)
   })
 
   socket.on('reconnect_failed', () => {
-    console.log('event: "reconnect_failed" ')
+    console.error('socket.io event: "reconnect_failed" ')
   })
 }
