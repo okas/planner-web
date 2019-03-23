ea3a0def8a714aa65597ea02b02941582afc879c
Lauri Saar <Lauri.Saar@telia.ee>
2019-03-24 00:52:31 +0200

    #Added Add Button. It is like Edit button, it's [disabled] state depends on API state.

---
+++ src/components/ButtonOk.vue
@@ -0,0 +1,19 @@
+<template functional>
+  <a
+    role="button"
+    class="ok button"
+    :disabled="data.disabled"
+    :class="[data.class, data.staticClass]"
+    v-bind="data.attrs"
+    v-on="listeners"
+  >
+    <i class="icon">
+      <fa-i class="fa-lg" icon="check" />
+    </i>
+  </a>
+</template>
+
+<script>
+import disabledIoNotConnected from '../mixins/disabled-ioNotConnected.js'
+export default { mixins: [disabledIoNotConnected] }
+</script>
