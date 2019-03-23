3be4b8c45a3bcaa52accdf9b173c2ff6267d6448
Lauri Saar <Lauri.Saar@telia.ee>
2019-03-24 00:56:58 +0200

    #Added Remove+Confirm Button. It can be used instead of deletion confirmation dialogue, because it asks confirmation. * exposes "confirm" event, it is fired when user has been clicked twice to the button; * changes icon and color between normal and confirmation asking states; * it is normal component (functional = false)

---
+++ src/components/ButtonRemoveConfirm.vue
@@ -0,0 +1,38 @@
+<template>
+  <a
+    role="button"
+    class="remove-confirm button"
+    :class="confirmed ? 'is-warning' : 'is-danger'"
+    :disabled="disabled"
+    v-bind="$attrs"
+    v-on="$listeners"
+    @click.prevent.stop="changeState"
+  >
+    <i class="icon">
+      <fa-i class="fa-lg" :icon="confirmed ? 'trash' : 'exclamation-circle'" />
+    </i>
+  </a>
+</template>
+
+<script>
+import disabledIoNotConnected from '../mixins/disabled-ioNotConnected.js'
+
+export default {
+  mixins: [disabledIoNotConnected],
+  data() {
+    return {
+      confirmed: true
+    }
+  },
+  watch: {
+    confirmed(newValue) {
+      if (newValue) this.$emit('confirm')
+    }
+  },
+  methods: {
+    changeState() {
+      this.confirmed = !this.confirmed
+    }
+  }
+}
+</script>
