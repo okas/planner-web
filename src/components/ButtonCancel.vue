f3e32834783decfcf49bbc590164e1704363623e
Lauri Saar <Lauri.Saar@telia.ee>
2019-03-24 00:51:28 +0200

    #Added Cancel Button compnent. It do not depend/use API state info to be set to disabled or endable.

---
+++ src/components/ButtonCancel.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <a
+    role="button"
+    class="cancel button"
+    :class="[data.class, data.staticClass]"
+    v-bind="data.attrs"
+    v-on="listeners"
+  >
+    <i class="icon">
+      <fa-i class="fa-lg" icon="times" />
+    </i>
+  </a>
+</template>
