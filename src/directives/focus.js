cf008a7b08beb7002eb3d2c82d146b84395ebd89
Lauri Saar <Lauri.Saar@telia.ee>
2019-03-25 02:24:27 +0200

    #Added argument support: * expects boolean argument value to set focus; * will blur element, if argument evaluates to false; * in case of component update, if new and oled values are same, then won't do anything.
    #Changes:
    - all DOM function calls are preceded with `await $nextTick()` calls.

--- src/directives/focus.js
+++ src/directives/focus.js
@@ -1,8 +1,23 @@
 export const name = 'focus'
 
 export const Focus = {
-  inserted(el) {
-    el.focus()
+  async inserted(el, binding, vNode) {
+    if (binding.expression == undefined || binding.value) {
+      await vNode.context.$nextTick()
+      el.focus()
+    }
+  },
+  async componentUpdated(el, binding, vNode) {
+    if (binding.value === binding.oldValue) {
+      return
+    }
+    if (binding.value) {
+      await vNode.context.$nextTick()
+      el.focus()
+    } else {
+      await vNode.context.$nextTick()
+      el.blur()
+    }
   }
 }
 
